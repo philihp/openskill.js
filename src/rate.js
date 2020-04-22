979c4a89f450397d5ae6c403636151556f9976bd
Philihp Busby <philihp@gmail.com>
2020-04-22 08:36:11 +0000

    more succinct way to write this

--- src/rate.js
+++ src/rate.js
@@ -1,11 +1,6 @@
 import models from './models'
 
-const rate = (teams, options = {}) => {
-  if (options.model === undefined) {
-    return models.plackettLuce(teams, options)
-  }
-  // const model = options?.model || 'plackettLuce'
-  return models[options.model](teams, options)
-}
+const rate = (teams, options = {}) =>
+  models[options?.model || 'plackettLuce'](teams, options)
 
 export default rate
