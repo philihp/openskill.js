2e43aa5927f464712ef28d45687ca8783b5c0875
Philihp Busby <philihp@gmail.com>
2020-04-22 09:05:45 +0000

    back to 100% coverage

--- src/rate.js
+++ src/rate.js
@@ -1,6 +1,6 @@
 import models from './models'
 
 const rate = (teams, options = {}) =>
-  models[options?.model || 'plackettLuce'](teams, options)
+  models[options.model || 'plackettLuce'](teams, options)
 
 export default rate
