c3da965020b395fe952f14b7c20017a2893b8d34
Philihp Busby <philihp@gmail.com>
2020-04-23 08:34:52 +0000

    add support for thurston-mosteller partial pairing model

--- src/models/index.js
+++ src/models/index.js
@@ -2,10 +2,12 @@ import plackettLuce from './plackettLuce'
 import bradleyTerryFull from './bradleyTerryFull'
 import bradleyTerryPart from './bradleyTerryPart'
 import thurstonMostellerFull from './thurstonMostellerFull'
+import thurstonMostellerPart from './thurstonMostellerPart'
 
 export default {
   plackettLuce,
   bradleyTerryFull,
   bradleyTerryPart,
   thurstonMostellerFull,
+  thurstonMostellerPart,
 }
