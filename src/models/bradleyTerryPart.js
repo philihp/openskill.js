5a6d9427c9079e8ab4a1ec3f46e92221c45604f0
Philihp Busby <philihp@gmail.com>
2020-04-23 04:32:06 +0000

    add support for bradley terry partial pairing model

---
+++ src/models/bradleyTerryPart.js
@@ -0,0 +1,38 @@
+import zip from 'array-zip'
+import { teamRating, score, ladderPairs } from '../util'
+import { BETASQ, EPSILON } from '../constants'
+
+const TWOBETASQ = 2 * BETASQ
+
+export default (game, _options) => {
+  const teamRatings = teamRating(game)
+  const adjacentTeams = ladderPairs(teamRatings)
+  return zip(teamRatings, adjacentTeams).map(([iTeamRating, iAdjacent]) => {
+    const [iMu, iSigmaSq, iTeam, iRank] = iTeamRating
+    const [iOmega, iDelta] = iAdjacent
+      .filter(([_qMu, _qSigmaSq, _qTeam, qRank]) => qRank !== iRank)
+      .reduce(
+        ([omega, delta], [qMu, qSigmaSq, _qTeam, qRank]) => {
+          const ciq = Math.sqrt(iSigmaSq + qSigmaSq + TWOBETASQ)
+          const piq = 1 / (1 + Math.exp((qMu - iMu) / ciq))
+          const sigSqToCiq = iSigmaSq / ciq
+          const gamma = Math.sqrt(iSigmaSq) / ciq
+
+          return [
+            omega + sigSqToCiq * (score(qRank, iRank) - piq),
+            delta + ((gamma * sigSqToCiq) / ciq) * piq * (1 - piq),
+          ]
+        },
+        [0, 0]
+      )
+    return iTeam.map(({ mu, sigma }) => {
+      const sigmaSq = sigma * sigma
+      return {
+        mu: mu + (sigmaSq / iSigmaSq) * iOmega,
+        sigma:
+          sigma *
+          Math.sqrt(Math.max(1 - (sigmaSq / iSigmaSq) * iDelta, EPSILON)),
+      }
+    })
+  })
+}
--- src/models/index.js
+++ src/models/index.js
@@ -1,7 +1,9 @@
 import plackettLuce from './plackettLuce'
 import bradleyTerryFull from './bradleyTerryFull'
+import bradleyTerryPart from './bradleyTerryPart'
 
 export default {
   plackettLuce,
   bradleyTerryFull,
+  bradleyTerryPart,
 }
