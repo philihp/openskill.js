c4c801c5fd28a8283ef3b0d2fc6a400127822606
Philihp Busby <philihp@gmail.com>
2020-06-24 00:43:39 +0000

    convert tests to use jest's toBeCloseTo

--- src/__tests__/statistics/vt.test.js
+++ src/__tests__/statistics/vt.test.js
@@ -10,10 +10,10 @@ describe('statistics#vt', () => {
   })
 
   it('with big b, small x', () => {
-    expect(vt(-1000, 1000)).toBe(0.7978845368663289)
+    expect(vt(-1000, 1000)).toBeCloseTo(0.79788)
   })
 
   it('with big b, big x', () => {
-    expect(vt(0, 1000) < 0.000000001).toBe(true)
+    expect(vt(0, 1000)).toBeCloseTo(0)
   })
 })
