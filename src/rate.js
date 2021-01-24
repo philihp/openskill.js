26947987d68ca42fcf7d840b86e02209df99a3ca
Philihp Busby <philihp@gmail.com>
2021-01-24 03:53:50 +0000

    support ranked ordering

--- src/rate.js
+++ src/rate.js
@@ -1,6 +1,7 @@
 import models from './models'
+import { reorder } from './util'
 
 const rate = (teams, options = {}) =>
-  models[options.model || 'plackettLuce'](teams, options)
+  models[options.model || 'plackettLuce'](reorder(options.rank)(teams))
 
 export default rate
