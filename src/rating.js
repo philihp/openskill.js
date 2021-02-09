df7d116a4d416115cddee9daa9cb56ed37d97d18
Philihp Busby <philihp@gmail.com>
2021-02-09 07:22:47 +0000

    fixes for different Z values

--- src/rating.js
+++ src/rating.js
@@ -1,8 +1,8 @@
 import { mu as defaultMu, sigma as defaultSigma } from './constants'
 
-const rating = ({ mu, sigma }, options = {}) => ({
+const rating = ({ mu, sigma } = {}, options = {}) => ({
   mu: mu ?? defaultMu(options),
-  sigma: sigma ?? defaultSigma(options),
+  sigma: sigma ?? defaultSigma({ ...options, mu }),
 })
 
 export default rating
