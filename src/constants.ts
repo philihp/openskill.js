b200bcb6aa1329a424961bcd9145a3fe1ac08d70
Philihp Busby <philihp@gmail.com>
2022-07-12 18:44:20 +0000

    fix build

--- src/constants.ts
+++ src/constants.ts
@@ -2,7 +2,8 @@ import { Options } from './types'
 
 export const z = (options: Options) => options?.z ?? 3
 export const mu = (options: Options) => options?.mu ?? 25
-export const sigma = (options: Options) => options?.sigma ?? mu(options) / z(options)
+export const sigma = (options: Options) =>
+  options?.sigma ?? mu(options) / z(options)
 
 export const epsilon = (options: Options) => options?.epsilon ?? 0.0001
 export const beta = (options: Options) => options?.beta ?? sigma(options) / 2
--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,5 @@ import rate from './rate'
 import ordinal from './ordinal'
 import predictWin from './predict-win'
 import predictDraw from './predict-draw'
-import { Rating, Options } from './types'
 
-export { rating, rate, ordinal, predictWin, predictDraw, Rating, Options }
+export { rating, rate, ordinal, predictWin, predictDraw }
