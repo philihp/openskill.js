febca3c238a3b0eb326181df182a19198388fa65
Philihp Busby <philihp@gmail.com>
2024-04-24 08:25:04 +0000

    rollback to eslint 8 because of typescript-eslint support

--- eslint.config.js
+++ eslint.config.js
@@ -1,1 +1,27 @@
-module.exports = []
+import eslint from '@eslint/js'
+import jestPlugin from 'eslint-plugin-jest'
+import tseslint from 'typescript-eslint'
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
+
+export default tseslint.config(
+  {
+    // config with just ignores is the replacement for `.eslintignore`
+    files: ['src/**/*.ts'],
+    ignores: ['**/dist/**', '**/benchmark/**', 'eslint.config.js'],
+    extends: [eslint.configs.recommended, eslintPluginPrettierRecommended, ...tseslint.configs.recommended],
+    rules: {
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          caughtErrorsIgnorePattern: '^_',
+        },
+      ],
+    },
+  },
+  {
+    files: ['src/**/__tests__/*.ts', 'src/**/*.test.ts'],
+    extends: [jestPlugin.configs['flat/recommended']],
+  }
+)
