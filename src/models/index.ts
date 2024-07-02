e03943fa9c88788fa5af75c17454e2d667248ef4
Philihp Busby <philihp@gmail.com>
2024-07-02 21:55:20 +0000

    refactor: index export pattern

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,7 +1,5 @@
-import plackettLuce from './plackett-luce'
-import bradleyTerryFull from './bradley-terry-full'
-import bradleyTerryPart from './bradley-terry-part'
-import thurstoneMostellerFull from './thurstone-mosteller-full'
-import thurstoneMostellerPart from './thurstone-mosteller-part'
-
-export { plackettLuce, bradleyTerryFull, bradleyTerryPart, thurstoneMostellerFull, thurstoneMostellerPart }
+export { default as plackettLuce } from './plackett-luce'
+export { default as bradleyTerryFull } from './bradley-terry-full'
+export { default as bradleyTerryPart } from './bradley-terry-part'
+export { default as thurstoneMostellerFull } from './thurstone-mosteller-full'
+export { default as thurstoneMostellerPart } from './thurstone-mosteller-part'
