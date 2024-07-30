40c4cb40eef54676c2fd53b5ad2160e9ee1cee20
Philihp Busby <philihp@gmail.com>
2024-07-30 19:48:29 +0000

    implementation of predictDraw

--- src/predict-draw.ts
+++ src/predict-draw.ts
@@ -1,36 +1,40 @@
-import { flatten } from 'ramda'
+import { flatten, sum, map, addIndex, reduce } from 'ramda'
 import constants from './constants'
-import util, { sum } from './util'
+import util, { TeamRating } from './util'
 import { phiMajor, phiMajorInverse } from './statistics'
 import { Options, Team } from './types'
 
-const predictWin = (teams: Team[], options: Options = {}) => {
+const predictDraw = (teams: Team[], options: Options = {}): number => {
   const { teamRating } = util(options)
   const { BETASQ, BETA } = constants(options)
 
-  const n = teams.length
-  if (n === 0) return undefined
-  if (n === 1) return 1
+  if (teams.length === 0) return 1
 
-  const denom = (n * (n - 1)) / (n > 2 ? 1 : 2)
-  const teamRatings = teamRating(teams)
-  const drawMargin = Math.sqrt(flatten(teams).length) * BETA * phiMajorInverse((1 + 1 / n) / 2)
+  const totalPlayerCount = flatten(teams).length
+  const drawProbability = 1 / totalPlayerCount
+  const drawMargin = Math.sqrt(totalPlayerCount) * BETA * phiMajorInverse((1 + drawProbability) / 2)
 
-  return (
-    Math.abs(
-      teamRatings
-        .map(([muA, sigmaSqA], i) =>
-          teamRatings
-            .filter((_, q) => i !== q)
-            .map(([muB, sigmaSqB]) => {
-              const sigmaBar = Math.sqrt(n * BETASQ + sigmaSqA + sigmaSqB)
-              return phiMajor((drawMargin - muA + muB) / sigmaBar) - phiMajor((muA - muB - drawMargin) / sigmaBar)
-            })
-        )
-        .flat()
-        .reduce(sum, 0)
-    ) / denom
+  const teamRatings = map<Team, TeamRating>((team) => teamRating([team])[0], teams)
+
+  const pairwiseProbs: number[] = addIndex<TeamRating, number[]>(reduce<TeamRating, number[]>)(
+    (qqq: number[], pairA: TeamRating, i: number): number[] => {
+      const [muA, sigmaSqA] = pairA
+      return reduce<TeamRating, number[]>(
+        (ppp: number[], pairB: TeamRating): number[] => {
+          const [muB, sigmaSqB] = pairB
+          const sharedDenom = Math.sqrt(totalPlayerCount * BETASQ + sigmaSqA + sigmaSqB)
+          ppp.push(phiMajor((drawMargin - muA + muB) / sharedDenom) - phiMajor((muB - muA - drawMargin) / sharedDenom))
+          return ppp
+        },
+        qqq,
+        teamRatings.slice(i + 1)
+      )
+    },
+    [],
+    teamRatings
   )
+
+  return sum(pairwiseProbs) / pairwiseProbs.length
 }
 
-export default predictWin
+export default predictDraw
